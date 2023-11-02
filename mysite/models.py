155e643f217849924b6591a655a505fa08e167af
linyu114 <11056006@ntub.edu.tw>
2023-11-02 12:41:51 +0800

    2023/11/2 12:41PM

--- mysite/models.py
+++ mysite/models.py
@@ -13,3 +13,36 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Postdetail(models.Model):
+    state=(
+        ("館藏中","館藏中"),
+        ("預約中","預約中"),
+        ("借出中","借出中"),
+    )
+    Bookname = models.CharField(max_length=200)
+    Author = models.CharField(max_length=100)
+    slug = models.CharField(max_length=200)
+    Content = models.TextField()
+    Publiccationdate = models.CharField(max_length=20)
+    State = models.CharField(max_length=8,choices=state)
+    
+    def __str__(self):
+        return self.Bookname
+    
+class PostdetailTwo(models.Model):
+    state=(
+        ("館藏中","館藏中"),
+        ("預約中","預約中"),
+        ("借出中","借出中"),
+    )
+    Bookname = models.CharField(max_length=200)
+    Author = models.CharField(max_length=100)
+    slug = models.CharField(max_length=200)
+    Content = models.TextField()
+    Publiccationdate = models.CharField(max_length=20)
+    State = models.CharField(max_length=8,choices=state)
+    Img = models.TextField() 
+    
+    def __str__(self):
+        return self.Bookname
--- mysite/views.py
+++ mysite/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from mysite.models import Post
+from mysite.models import Post,Postdetail,PostdetailTwo
 from datetime import datetime
 
 # Create your views here.
 def homepage(request):
-    posts = Post.objects.all()
+    #posts = Post.objects.all()
+    postdetails = Postdetail.objects.all()
     now = datetime.now()
     return render(request,'index.html',locals())
 
 def showpost(request,slug):
-    try:
-        post=Post.objects.get(slug=slug)
-        if post !=None:
-            return render(request,'post.html',locals())
-        else:
-            return redirect("/")
-    except:
-        return redirect('/')
+    post=Postdetail.objects.get(slug=slug)
+    if post !=None:
+        return render(request,'post.html',locals())
+    else:
+        return redirect("/")
